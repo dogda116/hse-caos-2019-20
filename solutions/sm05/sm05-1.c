62119b839f477150e68cbd959491c9ac8adafb99
Ruslan <appstoreoa1@gmail.com>
2020-01-08 15:43:26 +0300

    Create sm05-1.c

---
+++ solutions/sm05/sm05-1.c
@@ -0,0 +1,7 @@
+int mystrcmp(const char *str1, const char *str2) {
+    while (*str1 && *str1 == *str2) {
+        ++str1;
+        ++str2;
+    }
+    return *(const unsigned char *) str1 - *(const unsigned char *) str2;
+}
