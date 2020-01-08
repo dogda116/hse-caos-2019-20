4766fcfae0a3ed0b3f00946a992352b72e859e66
Ruslan <appstoreoa1@gmail.com>
2020-01-08 15:44:07 +0300

    Delete sm05-1.c

--- solutions/sm05/sm05-1.c
+++
@@ -1,7 +0,0 @@
-int mystrcmp(const char *str1, const char *str2) {
-    while (*str1 && *str1 == *str2) {
-        ++str1;
-        ++str2;
-    }
-    return *(const unsigned char *) str1 - *(const unsigned char *) str2;
-}
